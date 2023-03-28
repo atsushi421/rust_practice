039bd5259b75f434f1ab0460d962666a740a49d7
atsushi421 <yff81986@nifty.com>
2023-03-29 05:24:27 +0900

    feat: simple_trait

---
+++ simple_trait/src/main.rs
@@ -0,0 +1,64 @@
+trait TresureBox {
+    fn open(&self, key_no: i32) -> bool;
+    fn check(&self) -> i32;
+}
+
+struct JewelryBox {
+    key_no: i32,
+    price: i32,
+}
+
+impl TresureBox for JewelryBox {
+    fn open(&self, key_no: i32) -> bool {
+        if self.key_no == key_no {
+            true
+        }
+        else {
+            false
+        }
+    }
+    fn check(&self) -> i32 {
+        self.price
+    }
+}
+
+struct TrapBox {
+    key_no: i32,
+    damage: i32,
+}
+
+impl TresureBox for TrapBox {
+    fn open(&self, key_no: i32) -> bool {
+        if self.key_no == key_no {
+            true
+        }
+        else {
+            false
+        }
+    }
+    fn check(&self) -> i32 {
+        self.damage
+    }
+}
+
+#[test]
+fn test_jewelrybox() {
+    let jewelry_key_no = 30;
+    let jewelry_price = 20;
+    let jewelrybox = JewelryBox {key_no: jewelry_key_no, price: jewelry_price};
+
+    assert_eq!(jewelrybox.open(jewelry_key_no-1), false);
+    assert_eq!(jewelrybox.open(jewelry_key_no), true);
+    assert_eq!(jewelrybox.check(), jewelry_price);
+}
+
+#[test]
+fn test_trapbox() {
+    let trap_key_no = 30;
+    let trap_damage = -20;
+    let trapbox = TrapBox {key_no: trap_key_no, damage: trap_damage};
+
+    assert_eq!(trapbox.open(trap_key_no-1), false);
+    assert_eq!(trapbox.open(trap_key_no), true);
+    assert_eq!(trapbox.check(), trap_damage);
+}
