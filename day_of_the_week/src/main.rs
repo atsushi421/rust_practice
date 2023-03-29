cf8cd5d0e96aaa736f06a7ada39f0883f2a3ea26
atsushi421 <yff81986@nifty.com>
2023-03-30 08:13:51 +0900

    fix: bug

--- day_of_the_week/src/main.rs
+++ day_of_the_week/src/main.rs
@@ -9,6 +9,6 @@ fn test_get_kanji_strokes() {
     assert_eq!(Dow::Wednesday.get_kanji_strokes(), 4);
     assert_eq!(Dow::Thursday.get_kanji_strokes(), 4);
     assert_eq!(Dow::Friday.get_kanji_strokes(), 8);
-    assert_eq!(Dow::Sarurday.get_kanji_strokes(), 3);
+    assert_eq!(Dow::Saturday.get_kanji_strokes(), 3);
     assert_eq!(Dow::Sunday.get_kanji_strokes(), 4);
 }
